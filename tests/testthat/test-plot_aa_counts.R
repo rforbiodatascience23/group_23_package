39d4ca1d6e56ca0772400caecc0402ed9eb2bffe
AnaPasMed <s222761@dtu.dk>
2023-10-24 11:59:02 +0000

    Function 5 included for plotting frequency of aas

---
+++ tests/testthat/test-plot_aa_counts.R
@@ -0,0 +1,15 @@
+# test_that("multiplication works", {
+#   expect_equal(2 * 2, 4)
+# })
+#
+# test_that("plot_aa_counts test", {
+#   # Input sequence with amino acids
+#   seq <- "PVPGLKSSTGFVYFNATGSD"
+#
+#   # Call the function to generate the plot
+#   plot <- process_amino_acids(seq)
+#
+#   # Check if the result is a ggplot object
+#   expect_is(plot, "ggplot")
+#
+# })
