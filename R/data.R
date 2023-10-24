bfb8bc7e2cb537c41fb66efba6dace1d8d64a3f6
AnaPasMed <s222761@dtu.dk>
2023-10-24 08:31:48 +0000

    codon_table for package

---
+++ R/data.R
@@ -0,0 +1,7 @@
+#' Codon Table - Genetic Code
+#'
+#' Genetic code: codon and its corresponsing aminoacid
+#'
+#'
+#' @source \url{https://www.ncbi.nlm.nih.gov/Taxonomy/Utils/wprintgc.cgi?chapter=tgencodes#SG1}
+"codon_table"
